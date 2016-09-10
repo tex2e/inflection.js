af655b3a56ccab90295da5a423671a1b1c3cdd85
Mako <mnfeconicu41@gmail.com>
2016-09-10 22:58:44 +0900

    create index

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-'use strict';
+
+new Vue({
+  el: '#app',
+  data: {
+    selectedMode: 'Pluralize',
+    word: 'person',
+  },
+  computed: {
+    pluralized: function () {
+      return pluralize(this.word);
+    },
+    singularized: function () {
+      return singularize(this.word);
+    },
+  },
+});
